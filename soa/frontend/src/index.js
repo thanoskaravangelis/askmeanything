a86cd5f108752d26f50831291ff5972d73d5b24d
thanoskaravangelis <thanosblv@gmail.com>
2021-04-20 22:15:14 +0300

    Rename folder

---
+++ soa/frontend/src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch, Route, useParams } from "react-router-dom";
+import './index.css';
+import Splash from './Splash.js';
+import Landing from'./Landing.js';
+import SignUp from './SignUp.js';
+//import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/signin" exact>
+          <Splash />
+        </Route>
+        <Route path="/" exact>
+          <Landing />
+        </Route>
+        <Route path="/signup">
+          <SignUp />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
