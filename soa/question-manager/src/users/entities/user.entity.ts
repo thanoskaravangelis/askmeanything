a0fe0da0c7495bca3afee6d64042b48e1d7f0131
thanoskaravangelis <thanosblv@gmail.com>
2021-05-09 20:50:34 +0300

    Update users service

--- soa/question-manager/src/users/entities/user.entity.ts
+++ soa/question-manager/src/users/entities/user.entity.ts
@@ -1,5 +1,6 @@
 import {Column, Entity, OneToMany, PrimaryGeneratedColumn} from "typeorm";
 import {Question} from "../../question/entities/question.entity";
+import {Answer} from "../../answer/entities/answer.entity";
 
 @Entity()
 export class User {
@@ -41,4 +42,7 @@ export class User {
 
   @OneToMany( type => Question, question => question.user)
   questions: Question[];
+
+  @OneToMany( type => Answer , answer => answer.user)
+  answers: Answer[];
 }
