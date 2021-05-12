23784b47b424248449969f5d3ff313ce62f69aa1
Andreas Mantzoutas <anmantzoutas@gmail.com>
2021-05-12 14:40:46 +0300

    Fix app.module

--- soa/question-run/src/app.module.ts
+++ soa/question-run/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { QuestionModule } from './question/question.module';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import { AnswerModule } from './answer/answer.module';
+import { UserAnswerVoteModule } from './user-answer-vote/user-answer-vote.module';
+import { KeywordsModule } from './keywords/keywords.module';
+import { QuestionHasKeywordsModule } from './question-has-keywords/question-has-keywords.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, QuestionModule,TypeOrmModule.forRoot(), AnswerModule, UserAnswerVoteModule, KeywordsModule, QuestionHasKeywordsModule],
   controllers: [AppController],
   providers: [AppService],
 })
