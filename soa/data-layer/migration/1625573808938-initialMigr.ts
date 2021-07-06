405761741900d68b20dd28a965ca7b8cb48e55a8
thanoskaravangelis <thanosblv@gmail.com>
2021-07-06 15:47:42 +0300

    Add migration in data layer

---
+++ soa/data-layer/migration/1625573808938-initialMigr.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class initialMigr1625573808938 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
