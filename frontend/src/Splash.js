ff085185a727f70de00ca288f4ac3893f7649554
Andreas Mantzoutas <anmantzoutas@gmail.com>
2021-07-09 20:11:24 +0300

    Update splash page

--- frontend/src/Splash.js
+++ frontend/src/Splash.js
@@ -1,129 +1,83 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Splash.css';
-import { loginPost } from './api';
+import { loginPost, isLogged } from './api';
 
 
-class Splash extends React.Component {
+function Splash () {
     
-    constructor(props){
-        super(props);
-        this.state = {
-            username: "",
-            password: "",
-            submitOff: true,
-            userId:null,
-            logged:false,
-            error: null
-        }
-        this.handleInput = this.handleInput.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.formActive = this.formActive.bind(this);
-        this.homeRedirect = this.homeRedirect.bind(this);
-    }
-
-    componentDidMount() {}
-
-    homeRedirect = (e) => {
-        window.location.href = "/";
-    }
-
-    handleSubmit = (e) => {
-        console.log("submitted");
-        const params = new URLSearchParams();
-        params.append('username', this.state.username);
-        params.append('password', this.state.password);
-        loginPost(params)
-            .then(response => {
-                localStorage.setItem('token', response.data.access_token);
-                window.location.href = "/";
-            })
-            .catch(err => {
-                console.log(err);
-                this.setState({
-                    error : "Wrong username or password"
-                })
-            })
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(null);
+    const [success, setSuccess] = useState(null);
+    
+    useEffect(() => {
+        isLogged()
+        .then(response => {
+            window.location.href = '/';
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }, [])
+    
+    
+    const handleSubmit = (e) => {
         e.preventDefault();
-    }
-
-    handleInput = (e) => {
-        const name = e.target.name;
-        const value = e.target.value;
-        this.setState( {
-            [name]: value
+        console.log("submitted");
+        loginPost(username, password)
+        .then(response => {
+            localStorage.setItem('token', response.data.access_token);
+            setSuccess('Loggen in succesfully');
+            setTimeout(()=>{window.location.href='/'}, 200);
+        })
+        .catch(err => {
+            console.log(err);
+            setError("Wrong username or password");
         })
     }
 
-    formActive = (e) => {
-        const signinbut = document.getElementById("sign-in-button");
-        if (this.state.username==null || this.state.password==null) {
-            console.log("Cannot sign in due to missing fields.");
-            signinbut.classList.remove('active-btn');
-            this.setState({
-                submitOff:true
-            });
-        }
-        else{
-            if (this.state.username.length===0 || this.state.password.length===0) {
-                console.log("Cannot sign in due to missing fields.");
-                signinbut.classList.remove('active-btn');
-                this.setState({
-                    submitOff:true
-                });
-            }
-            else 
-            {
-                signinbut.classList.add('active-btn');
-                console.log("submit activated");
-                this.setState({
-                    submitOff:false
-                });
-            }
-        }
-        e.preventDefault();
-    }
-
-    render(){
-        if(this.state.logged) {
-            window.location.href = "/" ;
-        }
-        else{
-            return(
-                <div className="basic-container">
-                    <div className="main-splash-container center-content">
-                        <div id="logo"></div>
-                        <div className="title-container center-content">
-                            <div className="title-text" onClick={this.homeRedirect}>askmeanything</div>
-                        </div>
-                        <div>
-                            <span className="center-content" id="line-title"></span>
-                        </div>
-                        <div className="login-container center-content">
-                            <div id="form-container" className="center-content">
-                                <form>
-                                    <div className="login-form">
-                                        <label id="username-label">Username</label>
-                                        <input id="username-input" label="Username" name="username" value={this.state.username} onChange={this.handleInput} onKeyUp={this.formActive} type="text"/>
-                                        <label id="password-label">Password</label>
-                                        <input id="password-input" label="Password" name="password" value={this.state.password} onChange={this.handleInput} onKeyUp={this.formActive} type="password"/>
-                                    </div>
-                                    <input id="sign-in-button" type="submit" disabled={this.state.submitOff} value="Sign In" onClick={this.handleSubmit} />
-                                </form>
+    return (
+        <div className="basic-container">
+            <div className="main-splash-container center-content">
+                <div id="logo"></div>
+                <div className="title-container center-content">
+                    <div className="title-text" onClick={()=>{window.location.href='/'}}>askmeanything</div>
+                </div>
+                <div>
+                    <span className="center-content" id="line-title"></span>
+                </div>
+                <div className="login-container center-content">
+                    <div id="form-container" className="center-content">
+                        <div style={{'color': 'red'}}>{error}</div>
+                        <div style={{'color': 'green'}}>{success}</div>
+                        <form onSubmit={(event)=>{handleSubmit(event)}}>
+                            <div className="login-form">
+                                <label id="username-label">Username</label>
+                                <input id="username-input" label="Username" name="username" value={username} onChange={(event)=>{setUsername(event.target.value);setError(null);setSuccess(null);}} type="text"/>
+                                <label id="password-label">Password</label>
+                                <input id="password-input" label="Password" name="password" value={password} onChange={(event)=>{setPassword(event.target.value);setError(null);setSuccess(null);}} type="password"/>
                             </div>
-                        </div>
-                        <div className="or-register-container center-content">
-                            <div>
-                                <span className="center-content" id="line-title2"></span>
-                            </div>
-                            <div id="or-div">or</div>
-                            <div id="if-you-dont-div">If you don't have an account...</div>
-                            <a id="register-link" href="/signup">Sign Up Here</a>
-                        </div>
+                            <input id="sign-in-button"
+                                    type="submit"
+                                    disabled={!username || !password}
+                                    className={(!username || !password) ? '' : 'active-btn'}
+                                    value="Sign In"
+                                    onClick={handleSubmit} 
+                            />
+                        </form>
                     </div>
                 </div>
-            );
-        }
-    }
+                <div className="or-register-container center-content">
+                    <div>
+                        <span className="center-content" id="line-title2"></span>
+                    </div>
+                    <div id="or-div">or</div>
+                    <div id="if-you-dont-div">If you don't have an account...</div>
+                    <a id="register-link" href="/signup">Sign Up Here</a>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Splash;
