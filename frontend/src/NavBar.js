aa6ba2c878bdce242ca119fef9bad5d26fc1e64a
Andreas Mantzoutas <anmantzoutas@gmail.com>
2021-07-09 16:11:20 +0300

    Update navbar

--- frontend/src/NavBar.js
+++ frontend/src/NavBar.js
@@ -1,45 +1,87 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { isLogged } from './api';
 import './Landing.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import logo from './images/lightbulb_icon.png';
 
-class NavBar extends React.Component{
+function NavBar() {
 
-    constructor(props) {
-        super(props);
-        this.homeRedirect = this.homeRedirect.bind(this);
+    const [userId, setUserId] = useState(null);
+
+    useEffect(() => {
+        isLogged()
+        .then(response => {
+            setUserId(response.data.id);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }, [])
+
+    const logout = () => {
+        localStorage.setItem('token', null);
+        window.location.href='/signin';
     }
 
-    homeRedirect = (e) => {
-        window.location.href = "/";
-    }
 
-    redirectSgnUp = (e) => {
-        window.location.href = "/signup";
-    }
-
-    redirectSgnIn = (e) => {
-        window.location.href = "/signin";
-    }
-
-    render(){
-        return(
-            <div className="top-container">
-                    <div className="title-landing">
-                        <div id="logo-icon"></div>
-                        <div className="title-text" onClick={this.homeRedirect}>askmeanything</div>
-                    </div>
-                    <div className="gap-top" >
-                    </div>
-                    <div className="top-button-container">
-                        <div className="sign-in-container">
-                            <div id="sign-in-btn" onClick={this.redirectSgnIn}>Sign In</div>
-                        </div>
-                        <div className="sign-up-container">
-                            <button id="sign-up-btn" onClick={this.redirectSgnUp}>Sign Up</button>
-                        </div>
-                    </div>
+    return(
+        <Navbar bg="primary" variant="dark">
+            <Navbar.Brand href="#home">
+                <img className='logo-icon' src={logo}/>
+            </Navbar.Brand>
+            <Nav className="mr-auto">
+                <Nav.Link href="/">Home</Nav.Link>
+                {userId &&
+                    <Nav.Link href="/my">My AskMeAnything</Nav.Link>
+                }
+                <Nav.Link href='/questions'>Questions</Nav.Link>
+                {userId &&
+                    <Nav.Link href="/ask">Ask a question</Nav.Link>            
+                }
+            </Nav>
+            {userId &&
+                <Nav>
+                    <Nav.Link href={`/users/${userId}`}>Profile</Nav.Link>            
+                    <Nav.Link href="#" onClick={logout}>Logout</Nav.Link>
+                </Nav>
+            }
+            {!userId &&
+                <Nav>
+                    <Nav.Link href="/signin">Login</Nav.Link>
+                    <Nav.Link href="/signup">Register</Nav.Link>
+                </Nav>
+    
+            }
+        </Navbar>
+    )
+    return(
+        <div className="top-container">
+                <div className="title-landing">
+                    <div id="logo-icon"></div>
+                    <div className="title-text" onClick={()=>{window.location.href='/'}}>askmeanything</div>
                 </div>
-        );
-    }
+                <div className="gap-top" >
+                </div>
+                <div className="top-button-container">
+                    {!userId &&
+                        <div className="sign-in-container">
+                            <div id="sign-in-btn" onClick={()=>{window.location.href='/signin'}}>Sign In</div>
+                        </div>
+                    }
+                    {!userId &&
+                        <div className="sign-up-container">
+                            <button id="sign-up-btn" onClick={()=>{window.location.href='/signup'}}>Sign Up</button>
+                        </div>
+                    }
+                    {userId &&
+                        <div className="sign-up-container">
+                        <button id="sign-up-btn" onClick={logout}>Logout</button>
+                        </div>
+                    }
+                </div>
+            </div>
+    );
 }
 
 export default NavBar;
