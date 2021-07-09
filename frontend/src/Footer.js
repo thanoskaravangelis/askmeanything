d6baa78e00857319b27c70e30073a64cd606565b
Andreas Mantzoutas <anmantzoutas@gmail.com>
2021-07-09 15:50:21 +0300

    Update footers

--- frontend/src/Footer.js
+++ frontend/src/Footer.js
@@ -1,31 +1,32 @@
 import React from 'react';
 import './Footer.css';
-import gitlogo from './images/GitHub-Mark-64px.png';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
-class Footer extends React.Component{
-    constructor(props){
-        super(props);
-        this.gitRedirect=this.gitRedirect.bind(this);
-    }
-
-    gitRedirect = (e) => {
-        window.location.href='https://github.com/thanoskaravangelis/saas-80';
-    }
-
-    render() {
-        return(
-            <div className="footer-container">
-                <a className="footer-link">About</a>
-                <a className="footer-link">Contact Us</a>
-                <a className="footer-link">Project Documentation</a>
-                <div className="git-find-us center-content">
-                    <img id="git-logo" src={gitlogo} height="25px" onClick={this.gitRedirect}/>
-                    <a className="footer-link" id="git-footer-link" href="https://github.com/thanoskaravangelis/saas-80">Find us on GitHub</a>
-                </div>
-                <a className="footer-link">Course materials</a>
-            </div>
-        );      
-    }
+function Footer() {
+    return (
+        <Navbar bg="primary"
+                variant="dark"
+                expand="lg"
+                fixed='bottom'>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link style={{'width': '20vw'}} href="/about">About</Nav.Link>
+                    <Nav.Link style={{'width': '20vw'}} href="/contact">Contact us</Nav.Link>
+                    <Nav.Link style={{'width': '20vw'}} href="/">Documentation</Nav.Link>
+                    <Nav.Link style={{'width': '20vw'}} href="https://github.com/thanoskaravangelis/saas-80"
+                            rel='noopener noreferrer' target='_blank'>
+                        Github
+                    </Nav.Link>
+                    <Nav.Link style={{'width': '20vw'}} href="https://courses.pclab.ece.ntua.gr/course/view.php?id=34"
+                            rel='noopener noreferrer' target='_blank'>
+                        Course Materials
+                    </Nav.Link>
+                    </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    )
 }
 
 export default Footer;
